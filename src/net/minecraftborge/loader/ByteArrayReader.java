fefc6d3ffeb708ac2156b9a9c6bba11430cef087
Siepert123 <createlegacy69@gmail.com>
2026-06-25 20:20:44 +0200

    Updated byte array manipulators

--- src/net/minecraftborge/loader/ByteArrayReader.java
+++ src/net/minecraftborge/loader/ByteArrayReader.java
@@ -1,6 +1,9 @@
 package net.minecraftborge.loader;
 
-public class ByteArrayReader {
+import java.io.DataInput;
+import java.io.IOException;
+
+public class ByteArrayReader implements DataInput {
 	private byte[] data;
 	private int position = 0;
 	public ByteArrayReader() {}
@@ -10,33 +13,109 @@ public class ByteArrayReader {
 		this.position = 0;
 	}
 
+	@Override
 	public byte readByte() {
 		return this.data[this.position++];
 	}
+
+	@Override
 	public short readShort() {
 		short v = (short) ByteUtil.readShort(this.data, this.position);
 		this.position += 2;
 		return v;
 	}
+
+	@Override
 	public int readInt() {
 		int v = ByteUtil.readInt(this.data, this.position);
 		this.position += 4;
 		return v;
 	}
+
+	@Override
 	public long readLong() {
 		long v = ByteUtil.readLong(this.data, this.position);
 		this.position += 8;
 		return v;
 	}
+
+	@Override
+	public float readFloat() {
+		return Float.intBitsToFloat(this.readInt());
+	}
+
+	@Override
+	public double readDouble() {
+		return Double.longBitsToDouble(this.readLong());
+	}
+
+	@Override
+	public String readLine() throws IOException {
+		return readString(this, Short.MAX_VALUE);
+	}
+
+	@Override
+	public String readUTF() throws IOException {
+		return readString(this, Short.MAX_VALUE);
+	}
+
+	@Override
 	public int readUnsignedByte() {
 		return this.data[this.position++] & 255;
 	}
+
+	@Override
 	public int readUnsignedShort() {
 		int v = ByteUtil.readShort(this.data, this.position);
 		this.position += 2;
 		return v;
 	}
+
+	@Override
+	public char readChar() {
+		return (char) this.readShort();
+	}
+
+	@Override
+	public void readFully(byte[] b) {
+		for (int i = 0; i < b.length; i++) {
+			b[i] = this.readByte();
+		}
+	}
+
+	@Override
+	public void readFully(byte[] b, int off, int len) {
+		for (int i = 0; i < len; i++) {
+			b[i + off] = this.readByte();
+		}
+	}
+
+	@Override
+	public int skipBytes(int n) {
+		this.position += n;
+		if (this.position >= this.data.length) throw new ArrayIndexOutOfBoundsException(this.position);
+		return n;
+	}
+
+	@Override
 	public boolean readBoolean() {
 		return this.readByte() != 0;
+	}
+
+	public static String readString(DataInput data, int maxLength) throws IOException {
+		short len = data.readShort();
+		if(len > maxLength) {
+			throw new IOException("Received string length longer than maximum allowed (" + len + " > " + maxLength + ")");
+		} else if(len < 0) {
+			throw new IOException("Received string length is less than zero! Weird string!");
+		} else {
+			StringBuilder builder = new StringBuilder();
+
+			for(int i = 0; i < len; ++i) {
+				builder.append(data.readChar());
+			}
+
+			return builder.toString();
+		}
 	}
 }
--- src/net/minecraftborge/loader/ByteArrayWriter.java
+++ src/net/minecraftborge/loader/ByteArrayWriter.java
@@ -1,8 +1,10 @@
 package net.minecraftborge.loader;
 
+import java.io.DataOutput;
+import java.io.IOException;
 import java.util.Arrays;
 
-public class ByteArrayWriter {
+public class ByteArrayWriter implements DataOutput {
 	private final byte[] data;
 	private int position;
 
@@ -20,22 +22,89 @@ public class ByteArrayWriter {
 		return buf;
 	}
 
+	@Override
 	public void writeByte(int v) {
 		this.data[this.position++] = (byte) v;
 	}
+
+	@Override
 	public void writeShort(int v) {
 		ByteUtil.writeShort(this.data, this.position, v);
 		this.position += 2;
 	}
+
+	@Override
+	public void writeChar(int v) {
+		this.writeShort((char) v);
+	}
+
+	@Override
 	public void writeInt(int v) {
 		ByteUtil.writeInt(this.data, this.position, v);
 		this.position += 4;
 	}
+
+	@Override
 	public void writeLong(long v) {
 		ByteUtil.writeLong(this.data, this.position, v);
 		this.position += 8;
 	}
+
+	@Override
+	public void writeFloat(float v) {
+		this.writeInt(Float.floatToRawIntBits(v));
+	}
+
+	@Override
+	public void writeDouble(double v) {
+		this.writeLong(Double.doubleToRawLongBits(v));
+	}
+
+	@Override
+	public void writeBytes(String s) {
+		for (char c : s.toCharArray()) this.writeByte(c);
+	}
+
+	@Override
+	public void writeChars(String s) {
+		for (char c : s.toCharArray()) this.writeChar(c);
+	}
+
+	@Override
+	public void writeUTF(String s) throws IOException {
+		writeString(s, this);
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		this.writeByte(b);
+	}
+
+	@Override
+	public void write(byte[] b) throws IOException {
+		for (byte value : b) {
+			this.writeByte(value);
+		}
+	}
+
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		for (int i = 0; i < len; i++) {
+			this.writeByte(b[i+off]);
+		}
+	}
+
+	@Override
 	public void writeBoolean(boolean v) {
 		this.writeByte(v ? 1 : 0);
+	}
+
+	public static void writeString(String string, DataOutput data) throws IOException {
+		if(string.length() > Short.MAX_VALUE) {
+			throw new IOException("String too big");
+		} else {
+			data.writeShort(string.length());
+			data.writeChars(string);
+		}
 	}
 }
