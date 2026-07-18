12b3dd0df7c6b429fce9fcc3ccaf069359da67b2
Siepert123 <createlegacy69@gmail.com>
2026-07-18 17:07:31 +0200

    1.7.5 Meta-sensitive tile entities Custom dropped seeds for grass

--- src/net/minecraftborge/MinecraftBorge.java
+++ src/net/minecraftborge/MinecraftBorge.java
@@ -34,7 +34,7 @@ public class MinecraftBorge {
 	public static final String FALLBACK_UPDATE_URL = "https://minecraft-borge.github.io/files/versions.xml";
 	private static boolean useFallback = false;
 
-	public static final String VERSION = "1.7.4";
+	public static final String VERSION = "1.7.5";
 	public static final String MC_VERSION = "b1.7.3";
 
 	public static final Map<String, Object> blackboard = Collections.synchronizedMap(new HashMap<>());
