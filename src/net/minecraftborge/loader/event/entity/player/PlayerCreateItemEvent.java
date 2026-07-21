b87e98aef491904ce7bcd7327f119ed95c9a26e7
Siepert123 <createlegacy69@gmail.com>
2026-07-21 12:42:04 +0200

    1.8.0 PlayerCreateItemEvent now has a workstation parameter IRecipes can now perform logic when crafted

--- src/net/minecraftborge/loader/event/entity/player/PlayerCreateItemEvent.java
+++ src/net/minecraftborge/loader/event/entity/player/PlayerCreateItemEvent.java
@@ -7,11 +7,13 @@ import net.minecraft.src.World;
 public class PlayerCreateItemEvent extends PlayerEvent {
 	private final World world;
 	private final ItemStack stack;
+	private final String workstation;
 
-	public PlayerCreateItemEvent(ItemStack stack, World world, EntityPlayer entity) {
+	public PlayerCreateItemEvent(ItemStack stack, World world, EntityPlayer entity, String workstation) {
 		super(entity);
 		this.world = world;
 		this.stack = stack;
+		this.workstation = workstation;
 	}
 
 	public World getWorld() {
@@ -19,5 +21,8 @@ public class PlayerCreateItemEvent extends PlayerEvent {
 	}
 	public ItemStack getStack() {
 		return this.stack;
+	}
+	public String getWorkstation() {
+		return this.workstation;
 	}
 }
